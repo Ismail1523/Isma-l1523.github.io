2e3e3175f0cecf5c0f619e90afaabb5b91ca2e3d
Etudiant <2357626@carrfour.cegepvicto.ca>
2023-12-04 14:44:45 -0500

    version finale

--- js/main.js
+++ js/main.js
@@ -15,6 +15,10 @@ const winningCombos = [
 let board;
 let turn = 'X';
 let win;
+let parties;
+//sauvgarder les parties gagnés par X
+
+
 
 /*----- cached element references -----*/
 
@@ -27,9 +31,13 @@ document.getElementById('reset-button').addEventListener('click', init);
 
 
 /*----- functions -----*/
-
+/**
+ * 
+ * @returns 
+ */
 function getWinner() {
     let winner = null;
+    
     winningCombos.forEach(function(combo, index) {
         if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
         });
@@ -42,8 +50,11 @@ function handleTurn() {
     });
     board[idx] = turn;
     turn = turn === 'X' ? 'O' : 'X';
+    
     win = getWinner();
     render();
+    // Envoyer la valeur de win ( X ou O)
+    leGagnant(win);
 };
 
 function init() {
@@ -54,13 +65,38 @@ function init() {
     ];
     render();
 };
-
+/**
+ * x
+ */
 function render() {
     board.forEach(function(mark, index) {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `C'est une égalité, reine !` : win ? `${win} gagner le jeu!` : `C'est le tour de ${turn} !`;
     };
 
 init();
+//créer des variables 
+let gagnéX = document.getElementById('X');
+let gagnéO = document.getElementById('O');
+let nombreX = 0;
+let nombreO = 0;
+/**  function pour idetnfier le nombre de foix gagné pour x ou o
+ * @param {char} win la lettre qui a gagné
+ * si X est gagné:- nombre de foix gagnés +1
+ * - changer le texte dans le html avec le nombre de foix gagné
+ * si O est gagné:- nombre de foix gagné +1
+ * - changer le texte dans le html avec le nombre de foix gagné
+ * 
+ **/
+function leGagnant(win){
+    if(win == 'X'){
+        nombreX++;
+        gagnéX.innerHTML = "gagné par X :" + nombreX ;
+    }
+    else if(win == 'O'){
+        nombreO++;
+        gagnéO.innerHTML = "gagné par O :" + nombreO ;
+    }
+}
